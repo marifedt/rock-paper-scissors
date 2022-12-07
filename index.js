d1a848fa230bbc1df5f47e5780b6628a8d87a337
marifedt <57739727+marifedt@users.noreply.github.com>
2022-12-06 23:38:37 -0500

    Create logic for playRound() that returns result for the round

--- index.js
+++ index.js
@@ -7,6 +7,46 @@ const pick = ['ROCK', 'PAPER', 'SCISSORS'];
 let compChoice = getComputerChoice();
 let userPick = prompt("Rock, Paper or Scissors? ").toUpperCase();
 
+console.log(playRound(userPick, compChoice));
+
 function getComputerChoice(){
     return Math.floor(Math.random() * 3);
+}
+
+function playRound(playerSelection, computerSelection) {
+    let msg;
+    switch (playerSelection) {
+        case 'ROCK':
+            if(pick[computerSelection] === 'SCISSORS'){
+                msg = "You Win! Rock beats Scissors";
+            } else if(pick[computerSelection] === 'ROCK'){
+                msg = "It is a tie!";
+            } else{
+                msg = "You Lose! Paper beats Rock";
+            }
+            break;
+        case 'PAPER':
+            if(pick[computerSelection] === 'ROCK'){
+                msg = "You Win! Paper beats Rock";
+            } else if(pick[computerSelection] === 'PAPER'){
+                msg = "It is a tie!";
+            } else{
+                msg = "You Lose! Scissors beats Paper";
+            }
+            break;
+        case 'SCISSORS':
+            if(pick[computerSelection] === 'PAPER'){
+                msg = "You Win! Scissors beats Paper";
+            } else if(pick[computerSelection] === 'SCISSORS'){
+                msg = "It is a tie!";
+            } else{
+                msg = "You Lose! Rock beats Scissors";
+            }
+            break;
+        default:
+            msg = "Invalid choice. Choose between Rock, Paper or Scissors"
+            break;
+    }
+
+    return msg;
 }
