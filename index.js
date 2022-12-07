0dd1249837fe24406e9bdac23883d3699ba06eb5
marifedt <57739727+marifedt@users.noreply.github.com>
2022-12-06 23:41:08 -0500

    Put functions on top of function calls

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// for (let i = 0; i < 10; i++) {
-//     let choice = getComputerChoice();
-//     console.log(choice);
-// } Testing the input
-const pick = ['ROCK', 'PAPER', 'SCISSORS'];
-
-let compChoice = getComputerChoice();
-let userPick = prompt("Rock, Paper or Scissors? ").toUpperCase();
-
-console.log(playRound(userPick, compChoice));
-
 function getComputerChoice(){
     return Math.floor(Math.random() * 3);
 }
@@ -50,3 +39,13 @@ function playRound(playerSelection, computerSelection) {
 
     return msg;
 }
+
+
+const pick = ['ROCK', 'PAPER', 'SCISSORS'];
+
+let compChoice = getComputerChoice();
+let userPick = prompt("Rock, Paper or Scissors? ").toUpperCase();
+
+console.log(playRound(userPick, compChoice));
+
+
